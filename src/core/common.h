f2d9c8b13a98dc1c0d13995b7f61888e7ab4f317
Caner Karaca <canerkaraca_23@hotmail.com>
2026-07-30 02:29:33 +0300

    Fix C++20 std::lerp conflict by renaming lerp macro to lerp_val

--- src/core/common.h
+++ src/core/common.h
@@ -398,7 +398,7 @@ template<int s, int t> struct check_size {
 
 #define ABS(a)  (((a) < 0) ? (-(a)) : (a))
 #define norm_val(value, min, max) (((value) < (min)) ? 0 : (((value) > (max)) ? 1 : (((value) - (min)) / ((max) - (min)))))
-#define lerp(norm, min, max) ( (norm) * ((max) - (min)) + (min) )
+#define lerp_val(norm, min, max) ( (norm) * ((max) - (min)) + (min) )
 
 #define STRINGIFY(x)                    #x
 #define STR(x)                          STRINGIFY(x)
